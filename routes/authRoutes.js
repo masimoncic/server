15e7258169f64f14a3037d2c274be09bbc998e5a
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2021-05-06 15:02:55 -0400

    s

---
+++ routes/authRoutes.js
@@ -0,0 +1,45 @@
+
+const passport = require('passport');
+
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  })
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  })
+};
+
+
+
+/*const passport = require('passport');
+
+module.exports = (app) => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  )
+}
+
+
+module.exports = (app) => {
+  app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }))
+  
+}
+*/
+//app.get('/auth/google/callback', passport.authenticate('google'));
