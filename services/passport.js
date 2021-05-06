c1eac975a97311c2c4413593f6141758bb965746
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2021-05-06 17:03:27 -0400

    Update passport.js

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
-      poxy: true
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({  googleId: profile.id })
