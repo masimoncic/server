ac19e644364f977c810fd11b85810f5b2ec49d45
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2021-05-19 12:44:56 -0400

    a

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import Header from './Header'
+import Landing from './Landing'
 
-function App() {
-  return(
-    <div className='App'>
-      test
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return(
+    <div className='App container'>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path='/' component={Landing}/>
+            <Route exact path='/surveys' component={Dashboard}/>
+            <Route exact path='/surveys/new' component={SurveyNew}/>
+          </div>
+        </BrowserRouter>
     </div>
-  )
+    )
+  }
 }
 
-export default App;
+
+export default connect(null, actions)(App)
