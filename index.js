32b12ef001cbf696c0a8ff7c8bd18b38ef9a6f31
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2021-05-23 19:13:06 -0400

    update

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(express.json());
+
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -22,6 +24,18 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  //express will serve production assets
+  app.use(express.static('client/build'));
+
+  //express will serve up index.html if it doesnt recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
